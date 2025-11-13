3ebb8a4abfc7496da6661f4aec6d3ac409b922e7
SantOvalle08 <126879916+SantOvalle08@users.noreply.github.com>
2025-11-12 21:13:31 -0500

    factura

--- rentacar/front/files/src/utils/facturaGenerator.js
+++ rentacar/front/files/src/utils/facturaGenerator.js
@@ -180,7 +180,7 @@ export function generarFactura(reserva) {
     subtotal: subtotalGeneral,
     iva: ivaGeneral,
     total: totalGeneral,
-    totalLetras: numeroALetras(Math.floor(totalGeneral)) + ' PESOS COLOMBIANOS M/CTE',
+    totalLetras: numeroALetras(Math.floor(totalGeneral)) + ' USD',
     metodoPago: reserva.metodoPago || 'No especificado',
     periodo: {
       inicio: fechaInicio.toLocaleDateString('es-CO'),
