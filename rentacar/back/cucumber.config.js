4da6b86e3c62d0772023ae1492ab1a8fbc599fa9
Santiago Ovalle <santiago.ovallec@uqvirtual.edu.co>
2026-05-12 10:41:07 -0500

    chore: isolate generated test reports

--- rentacar/back/cucumber.config.js
+++ rentacar/back/cucumber.config.js
@@ -6,7 +6,7 @@ module.exports = {
       'features/support/world.js',
       'features/step_definitions/**/*.steps.js'
     ],
-    format: ['progress-bar', 'html:reports/cucumber-report.html'],
+    format: ['progress-bar', 'html:.test-reports/cucumber-report.html'],
     parallel: 1,
     timeout: 30000
   }
